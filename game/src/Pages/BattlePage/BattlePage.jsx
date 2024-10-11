3f5bdda4e7f5dcdf27c85d01b3b53a1e83b0aaff
Homayoun kolagar <homayounkolagar@gmail.com>
2024-10-11 18:17:32 +0330

    Added a new section that are the actions you can always do without losing a turn, they might change

--- game/src/Pages/BattlePage/BattlePage.jsx
+++ game/src/Pages/BattlePage/BattlePage.jsx
@@ -1,43 +1,44 @@
 import React from "react";
 import "./BattlePage.css";
 import actionCards from "../../database/cards/mainActionCards";
+import battleActions from "../../database/options/battleActions";
 
 export default function battlePage() {
     return (
         <main className="battle-page">
-            <section className="cards">
-                {actionCards.map((action) => (
-                    <div key={action.title} className="card">
-                        <div className="card-type">
-                            {action.type.toUpperCase()}
-                        </div>
-                        <h2 className="card-title">
-                            {action.title.toUpperCase()}
-                        </h2>
-                        <p className="card-description">{action.description}</p>
-                        <div className="card-details">
-                            {action.type === "attack" && (
+            <section className="main-container">
+                <section className="cards">
+                    {actionCards.map((action) => (
+                        <div key={action.title} className="card">
+                            <div className="card-type">
+                                {action.type.toUpperCase()}
+                            </div>
+                            <h2 className="card-title">
+                                {action.title.toUpperCase()}
+                            </h2>
+                            <p className="card-description">
+                                {action.description}
+                            </p>
+                            <div className="card-details">
+                                {action.type === "attack" && (
+                                    <span className="detail">
+                                        Base Damage: {action.details.baseDamage}
+                                    </span>
+                                )}
                                 <span className="detail">
-                                    Base Damage: {action.details.baseDamage}
+                                    Focus Cost: {action.details.focusCost}
                                 </span>
-                            )}
-                            <span className="detail">
-                                Focus Cost: {action.details.focusCost}
-                            </span>
-                            <span className="detail">
-                                CoolDown:{" "}
-                                {action.details.coolDown === "one time use"
-                                    ? "One-time use"
-                                    : `${action.details.coolDown} turn(s)`}
-                            </span>
-                            {action.details.healthRestore > 0 && (
                                 <span className="detail">
-                                    Health Restore:{" "}
-                                    {action.details.healthRestore}
+                                    CoolDown: {action.details.coolDown} turn(s)
                                 </span>
-                            )}
-                            {action.details.additionalEffects &&
-                                action.details.additionalEffects.length > 0 && (
+                                {action.details.healthRestore > 0 && (
+                                    <span className="detail">
+                                        Health Restore:{" "}
+                                        {action.details.healthRestore}
+                                    </span>
+                                )}
+                                {action.details.additionalEffects.length >
+                                    0 && (
                                     <span className="detail">
                                         Effects:{" "}
                                         {action.details.additionalEffects
@@ -45,12 +46,25 @@ export default function battlePage() {
                                             .join(", ")}
                                     </span>
                                 )}
-                            <span className="detail">
-                                Target Type: {action.details.targetType}
-                            </span>
+                                <span className="detail">
+                                    Target Type: {action.details.targetType}
+                                </span>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </section>
+
+                <section className="actions">
+                    <ul>
+                        {battleActions.map((action) => (
+                            <li key={action}>
+                                <button className="action-button">
+                                    {action}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
             </section>
         </main>
     );
---
+++ game/src/database/options/battleActions.js
@@ -0,0 +1,8 @@
+const battleActions = [
+    "Analyze Enemy",
+    "Check Enemy",
+    "Check Stats",
+    "Check Inventory",
+];
+
+export default battleActions;
