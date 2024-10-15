e338b72e459789a3751db5131e49c58aa7b04042
Homayoun kolagar <homayounkolagar@gmail.com>
2024-10-15 21:12:51 +0330

    Made focusCost be a positive number

--- game/src/database/cards/mainActionCards.js
+++ game/src/database/cards/mainActionCards.js
@@ -6,7 +6,7 @@ const actionCards = [
             "A powerful melee attack that inflicts damage and a bleeding effect.",
         details: {
             baseDamage: 150,
-            focusCost: -5,
+            focusCost: 5,
             coolDown: 1,
             healthRestore: 0,
             additionalEffects: [{ type: "bleed", duration: 3, damage: 10 }],
@@ -21,7 +21,7 @@ const actionCards = [
             "Creates a protective shield absorbing incoming damage but gives deBuffs.",
         details: {
             baseDamage: 0,
-            focusCost: -15,
+            focusCost: 15,
             coolDown: 3,
             healthRestore: 0,
             additionalEffects: [{ type: "shield", amount: 100 }],
@@ -35,7 +35,7 @@ const actionCards = [
         description: "Launch a projectile that damages and slows the target.",
         details: {
             baseDamage: 120,
-            focusCost: -10,
+            focusCost: 10,
             coolDown: 2,
             healthRestore: 0,
             additionalEffects: [{ type: "slow", duration: 1, amount: -10 }],
@@ -50,7 +50,7 @@ const actionCards = [
             "Drains energy from the enemy, dealing damage while restoring health.",
         details: {
             baseDamage: 100,
-            focusCost: -5,
+            focusCost: 5,
             coolDown: 3,
             healthRestore: 20,
             additionalEffects: [],
@@ -65,7 +65,7 @@ const actionCards = [
             "Bleeds to near death, giving a others [Confidence] and [Patience].",
         details: {
             baseDamage: 0,
-            focusCost: -20,
+            focusCost: 20,
             coolDown: "one time use",
             healthRestore: 0,
             additionalEffects: [{ type: "bleed", duration: 4 }],
